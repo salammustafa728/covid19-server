1f3bd3e13a4ca07f0516a2379791540302f600d7
salammustafa728 <salammustafa728@gmail.com>
2021-08-06 19:33:15 +0300

    edit the code delete unimportant things

--- server.js
+++ server.js
@@ -12,36 +12,13 @@ require('dotenv').config();
 
 const PORT= process.env.PORT;
 
-const {getData,getAllData}= require('./controller/cases.controller');
-
-
+const getAllData= require('./controller/cases.controller');
 
 app.get('/',(req,res)=>{
     res.send('workinnnnnnnnnnnnnnnnnnnnnnnnnnnng!');
 })
 
-// app.get('/art',getArtic);
-app.get('/cases',getData);
 app.get('/allData',getAllData);
-
-// mongoose.connect(process.env.MONGO_URL,
-//     { useNewUrlParser: true, useUnifiedTopology: true }
-// );
-
-
-
-//////////////////////////////////////////
-
-// create new fav art
-// app.post('/addcase',createCase);
-
-//  get fav art
-// app.get('/getFavCases',getFavCases);
-
-// delete fav art
-// app.delete('/deleteFavCases/:id',deleteFavCase);
-
-////////////////////////////////////////
 
 app.listen(PORT,()=>{
     console.log(`server working on ${PORT}`);
